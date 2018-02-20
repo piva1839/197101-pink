1de8dcd3f93b1fb04515492086957407db51e198
Oleksii <piva1839@gmail.com>
2018-02-20 07:27:03 +0200

    Работа над ошибками

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,115 +1,153 @@
 "use strict";
 
 module.exports = function(grunt) {
+  require("load-grunt-tasks")(grunt);
 
-require("load-grunt-tasks")(grunt);
+  grunt.initConfig({
+    clean: {
+      build: ["build"]
+    },
 
-grunt.initConfig({
-less: {
-style: {
-files: {
-"build/css/style.css": ["less/style.less"]
+    copy: {
+      build: {
+        files: [{
+          expand: true,
+          cwd: "source",
+          src: [
+            "fonts/**/*.{woff,woff2}",
+            "img/**",
+            "js/**",
+            "*.html"
+          ],
+          dest: "build"
+        }]
+      }
+    },
+
+    csso: {
+      style: {
+        options: {
+          report: "gzip"
+        },
+        files: {
+          "build/css/style.min.css": ["build/css/style.css"]
+        }
+      }
+    },
+
+    imagemin: {
+      images: {
+        options: {
+          optimizationLevel: 3,
+          progressive: true
+        },
+        files: [{
+          expand: true,
+          src: ["source/img/**/*.{png,jpg,svg}"]
+        }]
+      }
+    },
+
+    cwebp: {
+      images: {
+        options: {
+          q: 90
+        },
+        files: [{
+          expand: true,
+          src: ["source/img/**/*.{png,jpg}"]
+        }]
+      }
+    },
+
+    svgstore: {
+      options: {
+        includeTitleElement: false,
+        cleanup: ["fill","stroke"]
+      },
+      sprite: {
+        files: [{
+          "build/img/sprite.svg": ["source/img/icon-*.svg", "source/img/logo-htmlacademy.svg"]
+        }]
+      }
+    },
+
+    posthtml: {
+      options: {
+        use: [
+          require("posthtml-include")()
+        ]
+      },
+      html: {
+        files: [{
+          dot: true,
+          cwd: 'source/',
+          src: ['*.html'],
+          dest: 'build/',
+          expand: true,
+        }]
+      }
+    },
+
+    less: {
+      style: {
+        files: {
+          "build/css/style.css": "source/less/style.less"
+        }
+      }
+    },
+
+    postcss: {
+      style: {
+        options: {
+          processors: [
+            require("autoprefixer")()
+          ]
+        },
+        src: "build/css/*.css"
+      }
+    },
+
+    browserSync: {
+      server: {
+        bsFiles: {
+          src: [
+            "build/*.html",
+            "build/css/*.css"
+          ]
+        },
+        options: {
+          server: "build/",
+          watchTask: true,
+          notify: false,
+          open: true,
+          cors: true,
+          ui: false
+        }
+      }
+    },
+
+    watch: {
+      html: {
+        files: ["source/*.html"],
+        tasks: ["posthtml"]
+      },
+      style: {
+        files: ["source/less/**/*.less"],
+        tasks: ["less", "postcss", "csso"]
+      }
+    }
+  });
+
+  grunt.registerTask("serve", ["browserSync", "watch"]);
+
+  grunt.registerTask("build", [
+    "clean",
+    "copy",
+    "less",
+    "postcss",
+    "csso",
+    "svgstore",
+    "posthtml"
+  ]);
 }
-}
-},
-
-postcss: {
-style: {
-options: {
-processors: [
-require("autoprefixer")({browsers: [ "last 2 versions" ]})
-]
-},
-src: "build/css/*.css"
-}
-},
-
-csso: {
-style: {
-options: {
-report: "gzip"
-},
-files: {
-"build/css/style.min.css": ["build/css/style.css"]
-}
-}
-},
-
-copy: {
-build: {
-files: [{
-expand: true,
-src: [
-"fonts/**/*.{woff,woff2}",
-"img/**",
-"js/**",
-"*.html"
-],
-dest: "build"
-}]
-},
-html: {
-files: [{
-expand: true,
-src: ["*.html"],
-dest: "build"
-}]
-}
-},
-
-clean: {
-build: ["build"]
-},
-
-imagemin: {
-images: {
-options: {
-optimizationLevel: 3,
-progressive: true
-},
-files: [{
-expand: true,
-src: ["build/img/**/*.{png,jpg,gif,svg}"]
-}]
-}
-},
-
-browserSync: {
-server: {
-bsFiles: {
-src: [
-"build/*.html",
-"build/css/*.css"
-]
-},
-options: {
-server: "build/",
-watchTask: true
-}
-}
-},
-
-watch: {
-html: {
-files: ["*.html"],
-tasks: ["copy:html"]
-},
-style: {
-files: ["less/**/*.less"],
-tasks: ["less", "postcss", "csso"]
-}
-}
-
-});
-
-grunt.registerTask("serve", ["browserSync", "watch"]);
-
-grunt.registerTask("build", [
-"clean",
-"copy",
-"less",
-"postcss",
-"csso",
-"imagemin"
-]);
-};
